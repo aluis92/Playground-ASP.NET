2b55b382bfa42659d50c3c252af63a97361f5f94
aluis <andreluis9214@gmail.com>
2017-07-28 12:02:41 -0300

    show date on the main page

---
+++ Show date/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Show_date
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Show date/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Show_date.MvcApplication" Language="C#" %>
